25f6352277588458ccde8a47cac70b7ba70d4826
gdsamphier <gdssm@iinet.net.au>
2015-09-22 23:32:31 +1000

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object 
+## that stores a matrix and caches its inverse.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function creates a special 'matrix', which is really
+## a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the inverse of the matrix
+## 4. get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         invx <- NULL
         set <- function(y) {
+                ## store a new value for the matrix and
+                ## clear the previous inverse from cache
                 x <<- y
                 invx <<- NULL
         }
@@ -17,18 +24,23 @@ makeCacheMatrix <- function(x = matrix()) {
              getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function calculates the inverse of the special "matrix" created
+## with the makeCacheMatrix function. However, it first checks to see if the
+## inverse has already been calculated. If so, it gets the inverse from the cache
+## and skips the computation. Otherwise, it calculates the inverse of the matrix
+## and sets the value of the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         invx <- x$getinv()
         if(!is.null(invx)) {
+                ## inverse has already been calculated and cached; return that value 
                 message("getting cached inverse")
                 return(invx)
         }
+        ## inverse has not been calculated, calculate it, cache it, and return that value
         data <- x$get()
-        invx <- solve(data, ...)
+        invx <- solve(data, ...) ## for the exercise, assume that matrix has inverse 
         x$setinv(invx)
         invx
 }
